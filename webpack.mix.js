741d267a2dadcdfcc4a142892f9f3f22204edc6f
silehage <silehwage@gmail.com>
2022-08-30 15:50:10 +0700

    fix laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,8 +6,11 @@ require('mix-tailwindcss');
 
 mix.js('src/js/app.js', 'dist/js')
   .vue({ version: 3 })
+  .postCss('src/css/app.css', 'css')
+  .tailwind('./tailwind.config.js')
   .setPublicPath('dist')
   .version();
 
-mix.postCss('src/css/app.css', 'dist/css')
-  .tailwind('./tailwind.config.js');
+// mix.postCss('src/css/app.css', 'dist/css')
+//   .tailwind('./tailwind.config.js')
+//   .version();
